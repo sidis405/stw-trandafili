60c5f8bcc3eaddd71a3d491c804049250c676d7e
Sidrit Trandafili <forge405@gmail.com>
2023-03-28 00:46:30 +0200

    Commenti e readme

--- README.md
+++ README.md
@@ -1,24 +1,44 @@
-# stw-trandafili
+# Relazione sul progetto del generatore di tabelle CVE
+## Descrizione progettuale
+Il progetto consiste nella realizzazione di un componente Vue.js che permetta di generare una tabella HTML formattata contenente un elenco di indicatori univoci di vulnerabilità (CVE). L'obiettivo principale è fornire un'interfaccia semplice ed efficiente per l'inserimento e la visualizzazione di un elenco di CVE, supportando la validazione, la generazione di una tabella e l'esportazione dei dati in formato CSV o la copia in memoria.
+
+Il componente Vue.js è stato implementato utilizzando le seguenti tecnologie e librerie:
+
+Vue.js per la gestione delle funzionalità del componente e l'interazione con l'utente
+TailwindCSS per la formattazione e lo stile dell'interfaccia utente
+Jest e Vue Test Utils per i test di unità e la copertura del codice
+
+## Motivazioni delle scelte tecniche
+Vue.js è stato scelto come framework per lo sviluppo del componente, grazie alla sua semplicità, flessibilità e reattività. Vue.js permette di gestire facilmente lo stato e le interazioni dell'utente, garantendo al contempo una struttura modulare e manutenibile.
+
+TailwindCSS è stato selezionato come strumento per la formattazione dell'interfaccia utente grazie alla sua versatilità e alla vasta gamma di classi predefinite. Questo permette di creare rapidamente interfacce moderne e responsive senza dover scrivere molto codice CSS personalizzato.
+
+Per garantire la qualità e la robustezza del componente, sono stati scelti Jest e Vue Test Utils come strumenti per i test di unità. Jest offre un ambiente di test completo e flessibile, mentre Vue Test Utils fornisce una serie di utility per testare i componenti Vue.js in modo efficace.
+
+## Funzionalità non implementate e criticità incontrate
+Durante lo sviluppo del progetto, sono state implementate tutte le funzionalità richieste, tra cui la validazione degli input, la generazione della tabella, la copia in memoria e l'esportazione in formato CSV. Tuttavia, ci sono alcune funzionalità aggiuntive che potrebbero essere implementate per migliorare ulteriormente il componente:
+
+Migliorare l'accessibilità del componente, assicurandosi che sia compatibile con gli screen reader e supporti adeguatamente la navigazione da tastiera.
+Aggiungere una funzione di ricerca o filtraggio per permettere agli utenti di trovare facilmente specifici CVE nella tabella.
+Implementare la paginazione per gestire un gran numero di CVE, migliorando così le prestazioni e l'esperienza utente.
+Durante lo sviluppo del componente, non sono state riscontrate criticità significative. Tuttavia, è importante notare che i test per le funzioni copyTableToClipboard e downloadCSV sono difficili da eseguire a causa delle interazioni con il DOM e il sistema dei file, che non sono facilmente accessibili durante i test. Invece, sono stati testati i metodi generateTableText e generateCSVContent che vengono utilizzati da queste funzioni.
 
 ## Project setup
 ```
 npm install
 ```
 
-### Compiles and hot-reloads for development
+### Compilazione e and hot-reload per lo sviluppo
 ```
 npm run serve
 ```
 
-### Compiles and minifies for production
+### Compila per produzione
 ```
 npm run build
 ```
 
-### Run your unit tests
+### Esecuzione unit tests
 ```
 npm run test:unit
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/CveTable.vue
+++ src/components/CveTable.vue
@@ -1,13 +1,25 @@
+<!--
+Il template HTML del componente ToolCveList è diviso in diverse sezioni:
+- Un'intestazione fissa contenente il titolo della prova pratica.
+- Un titolo e una descrizione che spiegano la funzione del componente.
+- Una textarea per inserire i CVEs, che viene controllata per validare la formattazione.
+- Due menu a tendina per selezionare il numero di colonne e la larghezza della tabella.
+- Due pulsanti per generare la tabella e per copiarla negli appunti come testo tabulare o per scaricarla come file CSV.
+- La tabella generata, contenente la lista di CVEs.
+-->
 <template>
   <div class="p-4 container mx-auto">
+    <!-- Intestazione fissa -->
     <div class="fixed bottom-0 left-0 right-0 z-40 px-4 py-3 text-center text-white bg-gray-800">
       Prova pratica STW - Sidrit Trandafili
     </div>
+    <!-- Titolo e descrizione del tool -->
     <h2 class="text-base font-semibold leading-7 text-gray-900">Tool per la formattazione di una lista CVE</h2>
     <p class="mt-1 mb-2 text-sm leading-6 text-gray-600">Incolla la lista CVE, uno per riga, configura le opzioni e
       genera la tabella</p>
     <p class="mt-1 mb-2 text-sm leading-6 text-gray-600">A seguito potrai copiare la tabella in memoria o scaricarla
       come CSV</p>
+    <!-- Form per l'input della lista di CVE -->
     <div class="mb-4">
       <label for="about" class="block text-sm font-medium leading-6 text-gray-900">Lista CVE</label>
       <textarea
@@ -17,9 +29,11 @@
           @input="validateCVEs"
           :class="`block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6 ${invalidCVEs.length > 0 ? 'ring-2 ring-inset ring-red-600' : ''}`"
       ></textarea>
+      <!-- Messaggio di errore in caso di input non valido -->
       <p v-if="invalidCVEs.length > 0" class="mt-1 text-sm text-red-600">I seguenti CVE non sono validi:
         {{ invalidCVEs.join(', ') }}</p>
     </div>
+    <!-- Selezione del numero di colonne e larghezza della tabella -->
     <div class="flex mb-4 gap-3">
       <div class="mb-4 w-full">
         <label for="num-columns" class="block mb-2">Numero colonne:</label>
@@ -121,15 +135,20 @@
 export default {
   data() {
     return {
-      cveList: '',
-      numColumns: 3,
-      tableWidth: '100%',
-      tableData: [],
-      tableGenerated: false,
-      invalidCVEs: [],
+      cveList: '', //  è una stringa che rappresenta la lista di CVE da visualizzare nella tabella.
+      numColumns: 3, //  è un numero intero che rappresenta il numero di colonne che deve avere la tabella.
+      tableWidth: '100%', //  è una stringa che rappresenta la larghezza della tabella.
+      tableData: [], //  è una matrice che rappresenta i dati della tabella generati a partire dalla lista di CVE inserita.
+      tableGenerated: false, // è un booleano che indica se la tabella è stata generata o meno.
+      invalidCVEs: [], //  è una lista che contiene le CVE non valide inserite dall'utente.
     };
   },
   methods: {
+    // Questa funzione verifica la validità di ciascuna linea del testo di input CVE
+    // utilizzando un'espressione regolare che controlla se il formato della linea è corretto.
+    // Tutte le linee valide vengono salvate in un array, mentre le linee non valide vengono salvate
+    // in un altro array. Se vengono rilevate linee non valide, il valore di tableGenerated viene impostato su false.
+    // La funzione restituisce l'array di linee valide.
     validateCVEs() {
       const regex = /^CVE-\d{4}-\d{4,7}$/;
       const allLines = this.cveList.split('\n');
@@ -150,6 +169,12 @@ export default {
 
       return validCVEs;
     },
+
+    // Controlla se ci sono CVE non valide, e se ce ne sono restituisce subito.
+    // Successivamente valida le CVE tramite la funzione validateCVEs e, se valide,
+    // genera una matrice di oggetti row e value, dove ogni oggetto rappresenta una cella
+    // della tabella, e la salva nella variabile tableData.
+    // Infine, imposta il valore della variabile tableGenerated su true.
     generateTable() {
       if (this.invalidCVEs.length > 0) return;
 
@@ -176,6 +201,12 @@ export default {
       this.tableData = table;
       this.tableGenerated = true;
     },
+
+    // Questa funzione copia il contenuto della tabella negli appunti del sistema operativo
+    // creando un elemento textarea temporaneo e impostandone il valore con il contenuto della tabella,
+    // selezionando quindi il testo all'interno dell'elemento e copiandolo nella clipboard del sistema operativo,
+    // infine rimuove l'elemento textarea temporaneo.
+    // Nota: Questa funzione richiede l'accesso al DOM e non è testabile con Jest.
     copyTableToClipboard() {
       const tableText = this.generateTableText();
       const textArea = document.createElement('textarea');
@@ -185,6 +216,15 @@ export default {
       document.execCommand('copy');
       document.body.removeChild(textArea);
     },
+
+    // Questa funzione genera una stringa di testo che rappresenta la tabella dei CVE.
+    // Inizia creando una stringa vuota per contenere il testo della tabella.
+    // Itera attraverso le righe della tabella usando il metodo forEach(),
+    // per ogni riga itera attraverso le celle usando un altro forEach(),
+    // aggiungendo il valore di ogni cella alla stringa di testo della tabella,
+    // separando ogni valore di cella con una tabulazione "\t".
+    // Alla fine di ogni riga, aggiunge un carattere di ritorno a capo "\n" alla stringa di testo della tabella.
+    // Restituisce la stringa di testo della tabella generata.
     generateTableText() {
       let tableText = '';
       this.tableData.forEach(row => {
@@ -197,17 +237,38 @@ export default {
     },
 
     downloadCSV() {
+      // Genera il contenuto del file CSV
       const csvContent = this.generateCSVContent();
+
+      // Crea un nuovo Blob con il contenuto CSV e il tipo MIME specificato
       const blob = new Blob([csvContent], {type: 'text/csv;charset=utf-8;'});
+
+      // Crea un URL che rappresenta il Blob
       const url = URL.createObjectURL(blob);
+
+      // Crea un elemento di tipo anchor e gli assegna l'URL del Blob come href e il nome del file come download
       const link = document.createElement('a');
       link.setAttribute('href', url);
       link.setAttribute('download', 'table.csv');
+
+      // Imposta la visibilità del link a "hidden"
       link.style.visibility = 'hidden';
+
+      // Aggiunge il link alla pagina web
       document.body.appendChild(link);
+
+      // Simula il click del link per avviare il download
       link.click();
+
+      // Rimuove il link dalla pagina web
       document.body.removeChild(link);
     },
+
+    // Questa funzione genera il contenuto CSV della tabella
+    // Itera su ogni riga e colonna della tabella, aggiungendo ogni cella
+    // al contenuto CSV e sostituendo eventuali doppi apici nel valore
+    // della cella con due doppi apici per evitare errori nel formato CSV
+    // Restituisce il contenuto CSV generato come stringa
     generateCSVContent() {
       let csvContent = '';
       this.tableData.forEach(row => {
@@ -218,7 +279,6 @@ export default {
       });
       return csvContent;
     },
-
   },
 };
 </script>
